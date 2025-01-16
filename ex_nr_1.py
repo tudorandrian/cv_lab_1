31ac622f0c15605492699ccd061d9ed3fa99fd7c
tudorandrian <tudor.andrian5@gmail.com>
2025-01-16 17:24:15 +0200

    feat(io): add functionality to convert and save images in grayscale
    - Load an image in color mode from 'city_hall.jpg'.
    - Convert the color image to grayscale using cv2.cvtColor.
    - Save the resulting grayscale image as 'img_gray.jpg'.

--- ex_nr_1.py
+++ ex_nr_1.py
@@ -4,18 +4,6 @@ from matplotlib import pyplot as plt
 
 img = cv2.imread('city_hall.jpg',3)
 
-# cv2.imshow('image',img)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
-
-# cv2.namedWindow('image', cv2.WINDOW_NORMAL)
-# cv2.imshow('image',img)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
-
-# To save the image we will use the next function cv2.imwrite()
-# cv2.imwrite('img_gray.jpg',img)
-
 img = cv2.rectangle(img,(384,0),(510,128),(0,255,0),3)
 
 img = cv2.line(img,(0,0),(511,511),(255,0,0),5)
@@ -38,7 +26,6 @@ plt.imshow(img2)
 plt.xticks([]), plt.yticks([]) # to hide tick values on X and Y axis
 plt.show()
 
-# img = cv2.imread('city_hall.jpg',0)
 hist = cv2.calcHist([img],[0],None,[256],[0,256])
 
 plt.hist(img.ravel(),256,[0,256])
